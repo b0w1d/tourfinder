7ffa547f0720cf27e4e51f400ab9467a64851884
b0w1d <61a5t0@gmail.com>
2018-05-28 16:30:08 +0800

    Tour

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require 'line/bot'
 
+require 'open-uri'
+require 'nokogiri'
+
 def client
   @client ||= Line::Bot::Client.new { |config|
     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
@@ -8,7 +11,36 @@ def client
   }
 end
 
-$uids = {}
+def get_tour(lim)
+  $tused ||= {}
+
+  doc = Nokogiri::HTML(open("https://statsroyale.com/tournaments"))
+
+  frac = doc.xpath('//div[starts-with(@class, "challenges__row")]').map do |el|
+    u = el.children[0].text.split
+    u.empty? || u[0][0] == ?# ? nil : u[0]
+  end.compact
+
+  seri = doc.xpath('//div[starts-with(@class, "challenges__row")]').map do |el|
+    u = el.children[0].text.split
+    u.empty? || u[0][0] != ?# ? nil : u[0]
+  end.compact
+
+  name = doc.xpath('//a[starts-with(@class, "ui__blueLink")]').map do |el|
+    el.children[0].text
+  end
+
+  s = frac.zip(name.zip(seri)).map(&:flatten).map do |t|
+    f, n, x = t
+    a, b = f.split(?/).map &:to_i
+    a == b || b < lim || (lim > 50 && $tused.include?(x)) ? nil : ($tused[x] = true; t.join(?:))
+  end.compact.join(?\n)
+
+  return 'No tournament found' if s.empty?
+  s
+end
+
+$uids ||= {}
 
 post '/callback' do
   body = request.body.read
@@ -24,11 +56,11 @@ post '/callback' do
     when Line::Bot::Event::Message
       case event.type
       when Line::Bot::Event::MessageType::Text
-        $uids[event['source']['userId']] = true
         message = {
           type: 'text',
-          text: event.message['text']
+          text: get_tour(50)
         }
+        $uids[event['source']['userId']] = true
         client.reply_message(event['replyToken'], message)
       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
         response = client.get_message_content(event.message['id'])
@@ -41,11 +73,11 @@ post '/callback' do
   $uids.keys.each do |uid|
     message = {
       type: 'text',
-      text: uid
+      text: get_tour(100)
     }
-    p client.push_message(uid, message)
+
+    client.push_message(uid, message)
   end
-  sleep(20)
 
   "OK"
 end
