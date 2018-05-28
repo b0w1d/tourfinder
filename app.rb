12178a669533a602f36fcf807c9ca65d9f115e5c
b0w1d <61a5t0@gmail.com>
2018-05-28 16:44:55 +0800

    tour

--- app.rb
+++ app.rb
@@ -47,37 +47,41 @@ post '/callback' do
 
   signature = request.env['HTTP_X_LINE_SIGNATURE']
   unless client.validate_signature(body, signature)
-    error 400 do 'Bad Request' end
-  end
-
-  events = client.parse_events_from(body)
-  events.each { |event|
-    case event
-    when Line::Bot::Event::Message
-      case event.type
-      when Line::Bot::Event::MessageType::Text
+    # error 400 do 'Bad Request' end
+    t = get_tour(100)
+    if t != 'No tournament found' 
+      $uids.keys.each do |uid|
         message = {
           type: 'text',
-          text: get_tour(50)
+          text: get_tour(100)
         }
-        $uids[event['source']['userId']] = true
-        client.reply_message(event['replyToken'], message)
-      when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-        response = client.get_message_content(event.message['id'])
-        tf = Tempfile.open("content")
-        tf.write(response.body)
+
+        client.push_message(uid, message)
       end
     end
-  }
-
-  $uids.keys.each do |uid|
-    message = {
-      type: 'text',
-      text: get_tour(100)
+  else
+    events = client.parse_events_from(body)
+    events.each { |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: get_tour(50)
+          }
+          $uids[event['source']['userId']] = true
+          client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+          response = client.get_message_content(event.message['id'])
+          tf = Tempfile.open("content")
+          tf.write(response.body)
+        end
+      else
+      end
     }
 
-    client.push_message(uid, message)
+    "OK"
   end
 
-  "OK"
 end
